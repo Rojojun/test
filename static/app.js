9b58790a0260aa23b4bc03d4e26c43102679dddd
Hojun Na <zoloman316@gmail.com>
2022-04-25 17:49:28 +0900

    현재 위치 카카오지도 API사용 구현 완료 ex) 서울 금천구

--- static/app.js
+++ static/app.js
@@ -1,14 +1,42 @@
 /*window.onload(navigator.geolocation.getCurrentPosition((position) => {
     doSomething(position.coords.latitude, position.coords.longitude);
 })
-)
-function location(){
+)*/
+
+
+
+
+$(function(){
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition()
+        navigator.geolocation.getCurrentPosition(function(pos){
+
+            let long = pos.coords.longitude
+            let lat = pos.coords.latitude
+
+            $.ajax({
+                    url: 'https://dapi.kakao.com/v2/local/geo/coord2address.json?x=' + long + '&y=' + lat,
+                    type: 'GET',
+                    headers: {
+                        'Authorization': 'KakaoAK ac6a4068ede062398404530f4a813347'
+                    },
+                    success: function (data) {
+                    let road_city = data['documents']['0']['road_address']['region_1depth_name'];
+                    let road_region = data['documents']['0']['road_address']['region_2depth_name'];
+
+                        console.log(road_city, road_region);
+                        $('#current_location').text(road_city +' '+ road_region)
+                    },
+                    error: function(e) {
+                    console.log(e);
+                    }
+                });
+            })
+        }
+    else{
+        alert("error")
     }
 
-    $('#location').text()
-}*/
+})
 
 function search_result() {
     let result = $('#search').val()
